3b34fb71a18eb2a096203e7879f82bb10d9ab116
Somaie Kaveh <somaie.kaveh@gmail.com>
2024-08-12 09:55:24 +0330

    fix the issue of loading the images

--- shop.js
+++ shop.js
@@ -15,17 +15,15 @@ var shop = new Vue({
 	},
 	methods: {
 		showpic: function(pic) {
-			// const pics = {
-			// 	pic1 : '../picshop/exam1.jpg',
-			// 	pic2 : '../picshop/exam2.jpg',
-			// 	pic3 : '../picshop/exam3.jpg',
-			// 	pic4 : '../picshop/exam4.jpg',
-			// 	pic5 : '../picshop/exam5.jpg',
-			// 	pic6 : '../picshop/exam6.jpg',
-			// };
-			// this.currectimage = pics[pic];
-			this.currectimage = '';
-
+			const pics = {
+				pic1 : './picshop/exam1.jpg',
+				pic2 : './picshop/exam2.jpg',
+				pic3 : './picshop/exam3.jpg',
+				pic4 : './picshop/exam4.jpg',
+				pic5 : './picshop/exam5.jpg',
+				pic6 : './picshop/exam6.jpg',
+			};
+			this.currectimage = pics[pic];
 		}
 	}
 
