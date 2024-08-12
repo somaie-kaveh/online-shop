96c641f25d429e361950200f490cf6ef6e614e61
Somaie Kaveh <somaie.kaveh@gmail.com>
2024-08-12 15:20:02 +0330

    rewrite the html code using v-for

--- shop.js
+++ shop.js
@@ -11,19 +11,19 @@ Vue.component('div-box', {
 var shop = new Vue({
 	el:'#shopclothe',
 	data: {
+		pics: [
+			{id: 0, source: './picshop/exam0.jpg', size: 'm', height: '120', price: '80'},
+			{id: 1, source: './picshop/exam1.jpg', size: 'xl', height: '100', price: '170'},
+			{id: 2, source: './picshop/exam2.jpg', size: 'm', height: '120', price: '70'},
+			{id: 3, source: './picshop/exam3.jpg', size: 'l', height: '100', price: '120'},
+			{id: 4, source: './picshop/exam4.jpg', size: 'xl', height: '140', price: '130'},
+			{id: 5, source: './picshop/exam5.jpg', size: 's', height: '90', price: '200'},
+		],
 		currectimage: null,
 	},
 	methods: {
-		showpic: function(pic) {
-			const pics = {
-				pic1 : './picshop/exam1.jpg',
-				pic2 : './picshop/exam2.jpg',
-				pic3 : './picshop/exam3.jpg',
-				pic4 : './picshop/exam4.jpg',
-				pic5 : './picshop/exam5.jpg',
-				pic6 : './picshop/exam6.jpg',
-			};
-			this.currectimage = pics[pic];
+		showpic: function(env) {
+			this.currectimage = `./picshop/exam${env}.jpg`;
 		}
 	}
 
